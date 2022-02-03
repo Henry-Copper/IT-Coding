1eecccd16fe984891ad1d157bf559131dbdfde9e
Henry-Copper <92207658+Henry-Copper@users.noreply.github.com>
2022-02-03 14:31:12 +0200

    Added Exercise 2

---
+++ com/company/LU1/Exercise2_Greet.java
@@ -0,0 +1,16 @@
+// H Klopper 3 February 2022 LU1_Exercise2
+
+public class Exercise2_Greet
+{
+
+    static void main(String[] args)
+   {
+   
+   // incomplete
+   System.out.print("Hello");
+   System.out.println("How are you?");
+   System.out.println("My name is Sam");
+   
+   }
+   
+}
---
+++ com/company/LU1/Exercise2_TextOutput.java
@@ -0,0 +1,15 @@
+// H Klopper 1 February 2022 LU1_Exercise2
+
+public class Exercise2_TextOutput
+{
+
+   public static void main (String[] args)
+   {
+
+   System.out.print  ("Hello, 2 + 3 = "); // just "print" in order not to create a new line
+   System.out.print   (2 + 3);
+   System.out.print   (" Good Bye");
+   
+   }
+
+}
