4cd9fd3bdc9852bfdb5255b986bff832cec61271
Henry Copper <hennrycopper@gmail.com>
2022-02-04 08:12:47 +0200

    Completed Exercise 2

--- com/company/LU1/Exercise2_Greet.java
+++ com/company/LU1/Exercise2_Greet.java
@@ -3,14 +3,22 @@
 public class Exercise2_Greet
 {
 
-    static void main(String[] args)
+   public static void main(String[] args)
    {
    
-   // incomplete
-   System.out.print("Hello");
+   // first part
+   System.out.println("Hello");
    System.out.println("How are you?");
    System.out.println("My name is Sam");
    
+   // second part
+   System.out.print("I am going");
+   System.out.println(" to the");
+   System.out.println("shopping mall.");
+   System.out.println();
+   System.out.println("There is no bus");
+   System.out.print("may I have a lift?");
+
    }
    
 }
---
+++ com/company/Personal/test.java
@@ -0,0 +1,12 @@
+import it.*;
+
+public class test
+{
+
+   public static void main(String[] args)
+   {
+   Gogga.setGridSize(-10,-10);
+   Gogga bug = new Gogga();
+   }
+
+}
