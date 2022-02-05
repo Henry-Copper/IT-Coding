6783d5e549d467c02161c4f0f6ac03942cb8adc5
Henry Copper <hennrycopper@gmail.com>
2022-02-05 15:05:46 +0200

    Added and completed Exercise 4

--- com/company/LU1/Exercise4_HeartBeat.java
+++ com/company/LU1/Exercise4_HeartBeat.java
@@ -1,13 +1,60 @@
 // H Klopper 4 February 2022 LU1_Exercise4
 
+// create algorithm for this
+
+import it.*;
+
 public class Exercise4_HeartBeat
 {
+   
+   public static Gogga bug;
+   
+   private static int numSteps = 5;
 
    public static void main(String[] args)
    {
    
    
+   Gogga.setGridSize(20,10);
+   bug = new Gogga();  
+   bug.setPosition(1,9);
+   
+   repeatedBit(bug);
+   }
+   
+   public static void repeatedBit(Gogga critter)
+   {
+   
+   System.out.println(numSteps);
+   
+   for(int i = 0; i < numSteps; i++)
+   {
+   critter.move();
+   critter.turnRight();
+   critter.move();
+   critter.turnLeft();
+   }
+   
+   critter.move();
+   critter.turnRight();
+   critter.move();
+   critter.turnRight();
+   
+   numSteps++;
+   for (int i = 0; i < numSteps; i++)
+   {
+   critter.move();
+   }
+   
+   critter.turnLeft();
+   critter.move();
+   critter.move();
+   critter.turnLeft();
+   
+   numSteps = numSteps - 3;
+   if (numSteps > 0) repeatedBit(critter);
    
    }
+   
 
 }
