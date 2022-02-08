40219a938b1de2b9569b8e05972a63e1e994970f
Henry-Copper <92207658+Henry-Copper@users.noreply.github.com>
2022-02-08 11:11:06 +0200

    Added something

--- com/company/Grade 10/JLU2/Activity1_MakeLabels.java
+++ com/company/Grade 10/JLU2/Activity1_MakeLabels.java
@@ -1,8 +1,18 @@
 // H Klopper 8 February 2022 LU2_Activity1
 
+import java.io.*;
+
 public class Activity1_MakeLabels
 {
 
+   public static void main (String[] args)
+   {
+   
+   System.out.println("***************
+   System.out.println("***************
+   System.out.println("***************
+   System.out.println("***************
+   
+   }
 
-
-}
+} // class MakeLabels
