7229c36823cf5df8e2bff092ef3314ec29e26ad8
Henry Copper <hennrycopper@gmail.com>
2022-02-10 21:11:06 +0200

    added more c stuff; data types are weird

---
+++ Grade 10/Personal/C/floor and ceil test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main()
+{
+
+   // this produces 0???????
+   printf("hello %f", 3);
+
+}
---
+++ Grade 10/Personal/C/random number generator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+   // random number generator??????? -> every time it runs, different output?????? same compilation????????
+   double x = 3;
+   printf("hholland %d", x);
+
+}
+
