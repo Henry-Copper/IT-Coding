6ce1370d903a9311488d14a77b9f936bb9256583
Henry Copper <hennrycopper@gmail.com>
2022-02-14 16:17:31 +0000

    Added Checkpoint 2 stuff

--- Grade 10/LU1/Checkpoint2_ColouredLines.java
+++ Grade 10/LU1/Checkpoint2_ColouredLines.java
@@ -1,12 +1,32 @@
 // H Klopper 13 February 2022 LU1_Checkpoint2
 
+import it.*;
+import java.awt.*;
 
 public class Checkpoint2_ColouredLines
 {
 
         public static void main(String[] args)
         {
-        System.out.println("fuck");
-        }
+                Gogga louse = new Gogga();
+                louse.setPosition(2, 1);
+                louse.setDirection(Gogga.RIGHT);
+                louse.setColor(Color.black);
+                louse.move();
 
-}
+                louse.move();
+
+                louse.setPosition(2, 3);
+                louse.setDirection(Gogga.RIGHT);
+                louse.setColor(Color.blue);
+                louse.move();
+                louse.move();
+
+                // my bit
+                louse.setPosition(2, 5);
+                louse.setColor(Color.yellow);
+                louse.move();
+                louse.move();
+        } // main method
+
+} // Checkpoint2_ColouredLines class
