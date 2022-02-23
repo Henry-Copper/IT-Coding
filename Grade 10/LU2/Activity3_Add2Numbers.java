efaa7c15eb71ccee123b92c6de240016e9f6bbc6
Henry-Copper <92207658+Henry-Copper@users.noreply.github.com>
2022-02-23 09:59:58 +0200

    Added like stuff

--- Grade 10/LU2/Activity3_Add2Numbers.java
+++ Grade 10/LU2/Activity3_Add2Numbers.java
@@ -16,7 +16,14 @@ public class Activity3_Add2Numbers
    // have to put the integers in brackets to ensure they are added to each other, not just concatenated to the string from left to right
 
    System.out.println("The sum of the number is " + (num1 + num2));
-   
+  
+  
+  String test1 = "hello";
+  String test2 = "yes";
+   String test3 = "four";
+  
+  // doesn't matter in what order you concatenate; this runs 
+   System.out.println(test1 + (test2 + test3)); 
    } // main method
 
 } // Activity3_Add2Numbers class
