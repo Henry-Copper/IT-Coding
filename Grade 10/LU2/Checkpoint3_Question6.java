5429802e2cc20bc2218f7cd2ee467628198171e8
Henry Copper <hennrycopper@gmail.com>
2022-03-13 13:48:52 +0200

    Added LU2 Checkpoint 3

---
+++ Grade 10/LU2/Checkpoint3_Question6.java
@@ -0,0 +1,20 @@
+// H Klopper 13 March 2022 LU2_Checkpoint3
+
+
+
+public class Checkpoint3_Question6
+{
+
+   public static void main(String[] args)
+   {
+   
+   byte test1;
+   char test2;
+   int test3;
+   double test4;
+   
+   System.out.println(test1 + test2 + test3 + test4);
+   
+   }
+
+}
