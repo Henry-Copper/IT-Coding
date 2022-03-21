4216cebba658b024a5272a7e8600fd2be4650752
Henry-Copper <hennrycopper@gmail.com>
2022-03-21 12:44:11 +0200

    Checked checkpoint 4

---
+++ Grade 10/Personal/Nonsense/testingoprands.java
@@ -0,0 +1,19 @@
+public class testingoprands
+{
+
+   public static void main (String[] args)
+   {
+   
+   int num = 0;
+   num -= 1;
+   num--;
+   System.out.println(10/3.0);
+   double coolio = 10.0/3;
+   float floatyboi = 10/3.0f;
+   System.out.println(coolio);
+   System.out.println(floatyboi);
+   
+   System.out.println(13.0%4.0);
+   }
+
+}
