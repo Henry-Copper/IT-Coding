0c9e4d2196c3efea121ff14b82c585d85fd7814a
Henry-Copper <hennrycopper@gmail.com>
2022-03-22 13:50:41 +0200

    Added Activity 1

---
+++ Grade 10/LU3/Activity1_Characters.java
@@ -0,0 +1,25 @@
+// H Klopper 22 March 2022 LU3_Activity1
+
+public class Activity1_Characters
+{
+
+   public static void main(String[] args)
+   {
+   
+   // Part 1
+   /*
+   a
+   a
+   b
+   */
+   
+   // Part 2
+   char ch1 = 'a';
+   System.out.println(ch1);
+   char ch2 = 97;
+   System.out.println(ch2);
+   ch1++;
+   System.out.println(ch1);
+   }
+
+}
