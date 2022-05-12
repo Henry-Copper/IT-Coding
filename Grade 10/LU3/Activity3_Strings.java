09a07ff13d850fe9881f62efc512945fcf32ac1f
Henry-Copper <92207658+Henry-Copper@users.noreply.github.com>
2022-05-12 10:45:33 +0200

    Added activity 3

--- Grade 10/LU3/Activity3_Strings.java
+++ Grade 10/LU3/Activity3_Strings.java
@@ -6,7 +6,16 @@ public class Activity3_Strings
    public static void main (String [] args)
    {
    
+   String s1 = "hello";
+   String s2 = "there";
+   System.out.println(s1 + ' ' + s2); // one space between ' '
+   String s3 = "Line 1\nLine2";
+   System.out.println(s3);
+   String wrd = "" + s2.charAt(0)+s1.charAt(4);
    
+   System.out.println(wrd);
+   char ch = s2.charAt(3);
+   System.out.println(ch);
    
    }
 
